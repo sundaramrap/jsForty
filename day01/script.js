a9250cb6ab532e216878922529144f99722965cb
sundaramrap <pragya.learn.28@gmail.com>
2025-05-13 10:30:51 +0530

    'doneday1'

---
+++ day01/script.js
@@ -0,0 +1,4 @@
+// console.log(`this is extrnal javascript`);
+// document.writeln("check the browser console for a mesage!");
+console.log("Hello ,pragya");
+console.log("This is my 40days of Javascript ");
