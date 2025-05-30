b217636b81d97bb949214e6a4e8f74ec6a6f60da
sundaramrap <pragya.learn.28@gmail.com>
2025-05-30 15:47:27 +0530

    updatingday5

--- day05/reversedigi.js
+++ day05/reversedigi.js
@@ -2,9 +2,17 @@ console.log("Reverse digit using while loop");
 
 let input = 6789;
 
-let i = 0;
-let j = input.length - 1;
+let reversed = 0;
 
-while (i < j) {
-  let temp = 0;
+//formula --> last digit= number %10
+// addition of the digi = reverse *10 + lastdigit;
+
+while (input > 0) {
+  let lastdigi = input % 10;
+  reversed = reversed * 10 + lastdigi;
+  // eleminating the last digit from the input
+
+  input = Math.floor(input / 10);
 }
+
+console.log("The reverse is :" + reversed);
