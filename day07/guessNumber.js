568f6541b462fed8d6aa96c8e20c665cb4b49eb4
sundaramrap <pragya.learn.28@gmail.com>
2025-06-05 13:27:45 +0530

    Day7done

--- day07/guessNumber.js
+++ day07/guessNumber.js
@@ -1,1 +1,37 @@
+// console.log(sysNo);
 
+guessNumber = () => {
+  const sysNo = Math.floor(Math.random() * 10) + 1;
+  let userPromp = prompt("Enter a between 1-10 Number");
+  let userNumber = Number(userPromp);
+  let count = 1;
+  while (userNumber !== sysNo) {
+    if (userNumber > sysNo) {
+      console.log("Too high !!!, lower the value " + userNumber);
+      userPromp = prompt("Too high !!!, lower the value");
+    } else {
+      console.log("Too low !!!, increase the value " + userNumber);
+      userPromp = prompt("Too Low !!, increase the value");
+    }
+
+    userNumber = Number(userPromp);
+    count++;
+  }
+
+  console.log(
+    "Yay!!! " + userNumber + " guessed it correct in  " + count + " guess."
+  );
+
+  const replayGamePrompt = prompt("Play again? (yes/no)");
+
+  const replayGame = replayGamePrompt
+    ? replayGamePrompt.toLocaleLowerCase
+    : "no";
+  if (replayGame === "yes") {
+    guessNumber();
+  } else {
+    console.log("Thank you for playing");
+  }
+};
+
+guessNumber();
