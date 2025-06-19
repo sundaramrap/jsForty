a169eb96798a13b9d3a9cd717e86e69c432b8015
sundaramrap <pragya.learn.28@gmail.com>
2025-06-19 14:59:04 +0530

    'objaaaaaaandobjprops

--- day12/index.js
+++ day12/index.js
@@ -1,1 +1,19 @@
 console.log("This is day 12 file");
+// let user = {
+//   name: "Tehung",
+//   age: 45,
+// };
+
+// console.log(user.name);
+// user.isSeniorCitizen = false;
+// console.log(user);
+
+let user = {
+  name: "Tehung",
+  age: 45,
+  "is User Female": false,
+};
+console.log(user["is User Female"]);
+
+user["car edition"] = "BMW E5";
+console.log(user["car edition"]);
