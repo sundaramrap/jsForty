4844e8d0e1958f87edeaa5e339b845dd05a7852a
sundaramrap <pragya.learn.28@gmail.com>
2025-06-19 17:22:32 +0530

    dynamicassigning

--- day12/index.js
+++ day12/index.js
@@ -8,12 +8,25 @@ console.log("This is day 12 file");
 // user.isSeniorCitizen = false;
 // console.log(user);
 
-let user = {
-  name: "Tehung",
-  age: 45,
-  "is User Female": false,
-};
-console.log(user["is User Female"]);
+// let user = {
+//   name: "Tehung",
+//   age: 45,
+//   "is User Female": false,
+// };
+// console.log(user);
+// console.log(user["is User Female"]);
+// console.log(user.age);
+// user["car edition"] = "BMW E5";
+// console.log(user["car edition"]);
+// user.age = 34;
+// console.log(user.age);
+// // delete user["is User Female"];
+// // console.log(user);
+// let someUser = "age";
+// console.log(user[someUser]);
 
-user["car edition"] = "BMW E5";
-console.log(user["car edition"]);
+let car = prompt("Which is your fav car");
+let favCar = {
+  [car]: 5,
+};
+console.log(favCar);
