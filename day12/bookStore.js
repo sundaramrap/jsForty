6e43d00f96540b4dd15d5173335dc8f39fd795ad
sundaramrap <pragya.learn.28@gmail.com>
2025-06-23 20:13:52 +0530

    bookmanagement

--- day12/bookStore.js
+++ day12/bookStore.js
@@ -6,24 +6,55 @@
  * 2.check availabity and restock if stock is low
  */
 
-const bookInventory = {
-  "The Client": {
-    availability: false,
-    rating: 3.5,
-    stock: 0,
+const bookStore = {
+  Inventory: {
+    "The Client": {
+      availability: false,
+      rating: 3.5,
+      stock: 0,
+    },
+
+    "The Firm": {
+      availability: true,
+      rating: 4.2,
+      stock: 3,
+    },
+
+    "The Nutcracker and Mouseking": {
+      availability: true,
+      rating: 4.2,
+      stock: 10,
+    },
   },
 
-  "The Firm": {
-    availability: true,
-    rating: 4.2,
-    stock: 3,
+  availableBook: function (bookName) {
+    const book = this.Inventory[bookName];
+    if (!book) {
+      console.log(`${bookName} not availble in this place.`);
+      return;
+    }
+    if (book.stock > 0) {
+      console.log(
+        `Yes, ${bookName}  is available and stock is : ${book.stock}`
+      );
+    } else {
+      console.log(`${bookName} is  out of stock .`);
+    }
   },
 
-  "The Nutcracker and Mouseking": {
-    availability: true,
-    rating: 4.2,
-    stock: 10,
+  restockBook: function (bookName, quantity) {
+    book = this.Inventory[bookName];
+    if (!book) {
+      console.log(`Cannot Stock ${bookName}, not in Inventory.`);
+      return;
+    }
+
+    book.stock += quantity;
+    book.availability = true;
+    console.log(`${bookName} restocked to ${book.stock}.`);
   },
 };
 
-function bookcheck() {}
+bookStore.availableBook("The Client");
+bookStore.restockBook("The Client", 12);
+bookStore.availableBook("The Client");
