2fce319b4726631082fad8f33e71c853ff862daa
sundaramrap <pragya.learn.28@gmail.com>
2025-07-17 09:23:06 +0530

    'tilltask9day15'

--- day15/oneHr_Assignment.js
+++ day15/oneHr_Assignment.js
@@ -22,7 +22,47 @@ for (let i = 0; i < arrT3.length - 1; i++) {
 console.log(`_______-------TASK 05 ------_______`);
 //  T-005: Add one element at the front and the end of an array.
 
+arrT3.push("🦙");
+arrT3.unshift("🤦‍♀️🤦‍♀️");
+console.log(...arrT3);
+
+console.log(`_______-------TASK 06 ------_______`);
+
 //  T-006: Remove an element from the front and the end of an array.
+
+arrT3.pop();
+console.log(arrT3);
+arrT3.shift();
+console.log(arrT3);
 //  T-007: Create an array containing the name of your favourite foods(10 foods). Destructure the 6th food element from the array using destructuring.
+console.log(`_______-------TASK 07 ------_______`);
+/**Destructring means unpacking and giving distinct values */
+const favFood = [
+  "BaiganSabji",
+  "Udon",
+  "JaghpJage",
+  "Pretzel",
+  "Mandu",
+  "Crossotion",
+  "focaccia",
+  "bagel",
+  "ramen",
+  "gimbap",
+];
+console.log(favFood.length);
+[, , , , , sixthFood] = favFood;
+console.log(`My Favourite food is ${sixthFood}`);
+
 //  T-008: Take out the last 8 food items from the above array using the Array destructuring. Hint: rest parameter.
+
+console.log(`_______-------TASK 08 ------_______`);
+
+[a, b, ...rest] = favFood;
+
+console.log(`The remaing 8 Food items are ${rest}. `);
+
 //  T-009: Clone an Array(Shallow cloning)
+console.log(`_______-------TASK 09------_______`);
+
+let shallowFood = favFood.slice();
+console.log(shallowFood);
