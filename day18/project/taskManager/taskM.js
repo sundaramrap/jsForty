032bae4ff6e4c8a9c36f3ec1c6b4650200fb6ec4
sundaramrap <pragya.learn.28@gmail.com>
2025-08-02 15:28:30 +0530

    'tasllist_appending'

---
+++ day18/project/taskManager/taskM.js
@@ -0,0 +1,18 @@
+function listFill() {
+  const userInput = document.getElementById("taskInput");
+  const userList = document.getElementById("taskList");
+  const pInput = userInput.value;
+  if (pInput.trim() === "") return;
+  const li = document.createElement("li");
+  li.innerText = pInput;
+  userList.appendChild(li);
+}
+
+/**
+ *  OnClick of Button we will  add the task  written in the taskInput to the list .
+ * 1. adding function onClick on the button .
+ * 2.getting the value from the userInput Box
+ * 3. creating a li element
+ * 4.Appending li ement inside the ul tag.
+ *
+ */
