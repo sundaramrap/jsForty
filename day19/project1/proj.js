c905ae3e09b782214f4d4ddbb47a699bf741c5a2
sundaramrap <pragya.learn.28@gmail.com>
2025-11-09 12:03:26 +0530

    'proj1Comp'

--- day19/project1/proj.js
+++ day19/project1/proj.js
@@ -5,7 +5,17 @@ const mancomp = document.querySelector(".main-comp");
 //eventDelegation :Handle click on all the questions
 
 mancomp.addEventListener("click", function (e) {
-  const question = e.target.parentElement;
-  const answer = question.querySelector(".ans");
-  answer.classList.toggle("show");
+  if (e.target.classList.contains("que")) {
+    e.stopPropagation();
+    const question = e.target.parentElement;
+    const answer = question.querySelector(".ans");
+    answer.classList.toggle("show");
+  }
+});
+
+//to all the questins to collaspse when the windom is clicked apart from the question div , we will addEventListener at the top level and execute the logic there
+
+document.addEventListener("click", function () {
+  const allAnswer = document.querySelectorAll(".ans.show");
+  allAnswer.forEach((ans) => ans.classList.remove("show"));
 });
