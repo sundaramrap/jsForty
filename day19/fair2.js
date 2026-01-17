9c703d11a43862477a4758b454f88314f9eaa368
sundaramrap <pragya.learn.28@gmail.com>
2026-01-17 22:44:24 +0530

    jANUARY

--- day19/fair2.js
+++ day19/fair2.js
@@ -1,48 +1,10 @@
-//event- it is a singnal which ,tells something happend in the dom.
-function handleClick(greeting) {
-  console.log(`Button clicked with a ::-> ${greeting}`);
+console.log("...connected");
+
+//make two button and then  addd cclick handeler on it, also add evenlistener and removeEvent Listener from the panel
+function handleIncrement() {
+  console.log("button has been clicked");
 }
 
-const myBtn2Elem = document.getElementById("btnTwo");
+const buttonTwo = document.getElementById("btn2");
 
-myBtn2Elem.onclick = function () {
-  console.log("Btn is clicked ");
-};
-
-function handle2btnElem() {
-  console.log("The second fucntion is clicked here");
-}
-
-myBtn2Elem.addEventListener("click", handle2btnElem);
-//count function
-let count = 0;
-function handleCount() {
-  console.log("counter", count);
-  count++;
-}
-const incrementElement = document.getElementById("countBtn");
-incrementElement.addEventListener("click", handleCount);
-incrementElement.addEventListener("click", () => {
-  handleClick("rm");
-});
-incrementElement.removeEventListener("click", handleCount);
-
-//From line 16- 28 addeventlistener, removeeventlistener and handling multiple listener have been handled.
-
-//DOM contenet Loaded
-//some actions needs to be performed when a dom content is loaded, for which we use addEvenListener
-
-//what is inside eventObject
-//event object tells the user what , event is active on the current context which is being used
-
-const searchElement = document.getElementById("search-id");
-
-function handleSearchEvent(event) {
-  console.log(event);
-}
-
-searchElement.addEventListener("change", handleSearchEvent);
-
-//event bubbling, capturing and delegation
-
-//22ndNovember2025
+buttonTwo.on;
---
+++ day19/task1/task1.js
@@ -0,0 +1,8 @@
+//the content related to the tab's are hidden  through css
+// Tab should be selected all the tabs and then clicking on one of the particualr tab should represent the content of the selected tab which it has been assigned to .
+
+const tabS = document.querySelector(".tab");
+
+tabS.addEventListener("click");
+
+//noting down motivation evry day to get over the perception of failing out is also a struggle to keeping mind high and prosper .
