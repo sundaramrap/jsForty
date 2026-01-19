7e8c4463b4d55881e51a6ba75ea56ee3ddb6d7bf
sundaramrap <pragya.learn.28@gmail.com>
2026-01-19 23:45:52 +0530

    'toleftEventListenerCustom

--- day19/homeTask/main.js
+++ day19/homeTask/main.js
@@ -4,6 +4,10 @@ console.log("...connected");
 const mainSec = document.querySelector(".tabs");
 
 mainSec.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("tab")) return;
+
+  e.stopPropagation();
+
   if (e.target.classList.contains("tab")) {
     // console.log(`this is ${e.target.dataset.tab}`);
     // get the tab number and then run that tab number in the some other function and then call that function.
@@ -17,5 +21,10 @@ function showContent(tabNumber) {
   const tabCont = document.querySelectorAll(".tab");
   const conCont = document.querySelectorAll(".content");
 
-  tabCont.forEach((tab) => {});
+  tabCont.forEach((tab) => {
+    tab.classList.toggle("active", tab.dataset.tab === tabNumber);
+  });
+  conCont.forEach((content) => {
+    content.classList.toggle("active", content.dataset.tab === tabNumber);
+  });
 }
