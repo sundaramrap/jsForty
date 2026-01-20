bafdcfffe0217d3ab2584fe2ad8b4fe7d5e1e3c1
sundaramrap <pragya.learn.28@gmail.com>
2026-01-20 23:44:00 +0530

    'day20'

--- day20/index.js
+++ day20/index.js
@@ -1,12 +1,17 @@
 const parent = document.querySelector(".card");
 
 //efficient DOM traversal
-const firstC = parent.firstElementChild;
-console.log(firstC.innerText);
-const firstSib = firstC.nextElementSibling;
+// const firstC = parent.firstElementChild;
+// console.log(firstC.innerText);
+// const firstSib = firstC.nextElementSibling;
 
-console.log(firstSib.innerText);
+// console.log(firstSib.innerText);
 
 //Templates and Cloning
 
 const template = document.getElementById("card-template");
+//cloneNode to clone the node from the template
+const clone = template.content.cloneNode(true);
+clone.querySelector(".title").textContent = "DOM Advanced Topic";
+
+document.body.appendChild(clone);
