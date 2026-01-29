390bd18be7daddfc6ac012a6785abf64976763c6
sundaramrap <pragya.learn.28@gmail.com>
2026-01-29 09:20:17 +0530

    'class21'

--- day20/index.js
+++ day20/index.js
@@ -15,3 +15,19 @@ const clone = template.content.cloneNode(true);
 clone.querySelector(".title").textContent = "DOM Advanced Topic";
 
 document.body.appendChild(clone);
+
+//shadow DOM (different Subject just a jist )
+
+document.querySelector(".card").innerHTML;
+//selecting the inner element of the card to  shadow paste it
+
+//shadow Host
+//does not reside in the main dom , it is isolated and existance comes to life when added
+const shadowHost = document.getElementById("box");
+//attaching shadow to the element div
+
+const shadow = shadowHost.attachShadow({ mode: "open" });
+//attaching content to the inner structure of the Shadow box.
+shadow.innerHTML = `<style>p {color:red;}</style> <p>Hello Shadow</p>`;
+
+//advanced class manipulation
